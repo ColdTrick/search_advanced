605ecd431dac657bca626187320a9c0f47a2ba2b
jdalsem <jdalsem@coldtrick.com>
2012-07-31 10:12:14 +0200

    fixed: dropdown contents now show correct users/groups

--- procedures/autocomplete.php
+++ procedures/autocomplete.php
@@ -1,7 +1,7 @@
 <?php
 
 $q = sanitise_string(get_input("q"));
-$limit = (int) get_input("limit", 50);
+$limit = max((int) get_input("limit", 5), 1);
 $page_owner_guid = (int) get_input("page_owner_guid");
 
 $result = array();
@@ -12,23 +12,40 @@ if(!empty($q)){
 	// look for users
 	$options = array(
 		"type" => "user",
-		"limit" => 5,
+		"limit" => $limit,
 		"joins" => array("JOIN " . elgg_get_config("dbprefix") . "users_entity ue ON e.guid = ue.guid"),
 		"wheres" => array("(ue.name like '%" . $q . "%' OR ue.username like '%" . $q . "%')")
 	);
 	
 	if($logged_in_user){
 		// look only in friends
-		$options["relationship"] = "friend_of";
+		$options["relationship"] = "friend";
 		$options["relationship_guid"] = $logged_in_user->getGUID();
 		$users = elgg_get_entities_from_relationship($options);
 	}
 	
-	if(!$users){
-		// no friends or logged out
+	if(!$users || (count($users) < $limit)){
+		// no friends or logged out or fill up to limit
+		if($users){
+			// exclude already found users
+			$options["limit"] = $limit - count($users);
+			$guids = array();
+			foreach($users as $tmp_user){
+				$guids[] = $tmp_user->getGUID();
+			}
+			$options["wheres"][] = "e.guid NOT IN (" . implode(",", $guids) . ")";
+		}
 		$options["relationship"] = "member_of_site";
 		$options["relationship_guid"] = elgg_get_site_entity()->getGUID();
-		$users = elgg_get_entities($options);
+		$options["inverse_relationship"] = true;
+		
+		if($site_users = elgg_get_entities_from_relationship($options)){
+			if($users){
+				$users = array_merge($users, $site_users);
+			} else {
+				$users = $site_users;
+			}
+		}
 	}
 	
 	if($users){
@@ -41,7 +58,7 @@ if(!empty($q)){
 	// search for groups
 	$options = array(
 		"type" => "group",
-		"limit" => 5,
+		"limit" => $limit,
 		"joins" => array("JOIN " . elgg_get_config("dbprefix") . "groups_entity ge ON e.guid = ge.guid"),
 		"wheres" => array("ge.name like '%" . $q . "%'")
 	);
@@ -53,11 +70,28 @@ if(!empty($q)){
 		$groups = elgg_get_entities_from_relationship($options);
 	}
 	
-	if(!$groups){
+	if(!$groups || (count($groups) < $limit)){
 		// no groups or logged out
+		if($groups){
+			// exclude already found groups
+			$options["limit"] = $limit - count($groups);
+			$guids = array();
+			foreach($groups as $tmp_group){
+				$guids[] = $tmp_group->getGUID();
+			}
+			$options["wheres"][] = "e.guid NOT IN (" . implode(",", $guids) . ")";
+		}
+		
 		unset($options["relationship"]);
 		unset($options["relationship_guid"]);
-		$groups = elgg_get_entities($options);
+		
+		if($site_groups = elgg_get_entities($options)){
+			if($groups){
+				$groups = array_merge($groups, $site_groups);
+			} else {
+				$groups = $site_groups;
+			}
+		}
 	}
 	
 	if($groups){
