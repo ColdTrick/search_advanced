65476c0590068616cd0bad0a6f2c84e04e5f164b
Jeroen Dalsem <jdalsem@coldtrick.com>
2012-10-11 12:16:56 +0200

    remove duplicatie pages_top from type selection

--- views/default/search_advanced/search/type_selection.php
+++ views/default/search_advanced/search/type_selection.php
@@ -3,26 +3,35 @@ $types = get_registered_entity_types();
 
 $current_selected = elgg_echo("all");
 
-$entity_type = get_input("entity_type");
-$entity_subtype = get_input("entity_subtype");
-
-if(!empty($entity_type)){
-	echo elgg_view("input/hidden", array("name" => "search_type", "value" => "entities"));
-	echo elgg_view("input/hidden", array("name" => "entity_type", "value" => $entity_type));
+$search_type = get_input("search_type");
+if($search_type == "tags"){
+	$current_selected = elgg_echo("search_types:" .  $search_type);
 	
-	$current_selected = elgg_echo("item:" . $entity_type);
-	
-	if(!empty($entity_subtype)){
-		$current_selected = elgg_echo("item:" . $entity_type . ":" . $entity_subtype);
-		
-		echo elgg_view("input/hidden", array("name" => "entity_subtype", "value" => $entity_subtype));
-	} else {
-		echo elgg_view("input/hidden", array("name" => "entity_subtype", "disabled" => "disabled"));
-	}	
-} else {
-	echo elgg_view("input/hidden", array("name" => "search_type", "value" => "entities","disabled" => "disabled"));
+	echo elgg_view("input/hidden", array("name" => "search_type", "value" => $search_type));
 	echo elgg_view("input/hidden", array("name" => "entity_type", "disabled" => "disabled"));
 	echo elgg_view("input/hidden", array("name" => "entity_subtype", "disabled" => "disabled"));
+} else {
+	$entity_type = get_input("entity_type");
+	$entity_subtype = get_input("entity_subtype");
+	
+	if(!empty($entity_type)){
+		echo elgg_view("input/hidden", array("name" => "search_type", "value" => "entities"));
+		echo elgg_view("input/hidden", array("name" => "entity_type", "value" => $entity_type));
+		
+		$current_selected = elgg_echo("item:" . $entity_type);
+		
+		if(!empty($entity_subtype)){
+			$current_selected = elgg_echo("item:" . $entity_type . ":" . $entity_subtype);
+			
+			echo elgg_view("input/hidden", array("name" => "entity_subtype", "value" => $entity_subtype));
+		} else {
+			echo elgg_view("input/hidden", array("name" => "entity_subtype", "disabled" => "disabled"));
+		}	
+	} else {
+		echo elgg_view("input/hidden", array("name" => "search_type", "value" => "entities","disabled" => "disabled"));
+		echo elgg_view("input/hidden", array("name" => "entity_type", "disabled" => "disabled"));
+		echo elgg_view("input/hidden", array("name" => "entity_subtype", "disabled" => "disabled"));
+	}
 }
 
 ?>
@@ -39,6 +48,9 @@ if(!empty($entity_type)){
 			<li>
 				<a rel='group'><?php echo elgg_echo("item:group");?></a>
 			</li>
+			<li>
+				<a rel='tags'><?php echo elgg_echo("search_types:tags");?></a>
+			</li>
 			<?php 
 			/*
 			 <li>
@@ -48,6 +60,10 @@ if(!empty($entity_type)){
 			
 			 */ 
 				foreach($types["object"] as $subtype){
+					if($subtype === "page_top"){
+						// skip this one as it is merged with page objects
+						continue;
+					}
 					echo "<li><a rel='object " . $subtype . "'>" . elgg_echo("item:object:" . $subtype) . "</a></li>";
 				}
 			/*?>
