3b02d448b922ef4ac2e24817bf4d7e6766e164db
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-01-30 16:22:24 +0100

    updated translations

--- languages/nl.php
+++ languages/nl.php
@@ -1,9 +1,14 @@
 <?php
-
-	$dutch = array(
-			'search_advanced:multisite:label' => "Zoeken in",
-			'search_advanced:multisite:current' => "huidige site",
-			'search_advanced:multisite:mine' => "al mijn sites",
-	);
-		
-	add_translation("nl",$dutch);
+$language = array (
+  'search_advanced:settings:combine_search_results' => 'Combineer zoekresultaten',
+  'search_advanced:settings:combine_search_results:info' => 'Dit zal een gecombineerde lijst van de meest recente content tonen op basis van een zoekopdracht. Er zal geen groepering meer zijn op de resultaten pagina. Filteren middels het menu is nog steeds mogelijk.',
+  'search_advanced:settings:enable_multi_tag' => 'Multi tag search activeren',
+  'search_advanced:settings:enable_multi_tag:info' => 'Indien er een komma gebruikt wordt in een zoek opdracht zal deze behandeld worden als een aparte query. De resultaten zullen worden gecombineerd. De komma werkt dan als een OF selectie. Dit is niet van toepassing op de autocomplete zoekfunctionaliteit.',
+  'search_advanced:content:title' => 'Content',
+  'search_advanced:content:last_updated' => 'Laatst geüpdate',
+  'search_advanced:content:created' => 'Aangemaakt',
+  'search_advanced:multisite:label' => 'Zoeken in',
+  'search_advanced:multisite:current' => 'huidige site',
+  'search_advanced:multisite:mine' => 'al mijn sites',
+);
+add_translation("nl", $language);
