f629446458020b2850dd54857ac86b45ca484ac1
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-02-07 10:49:20 +0100

    only explode if there are types defined

--- views/default/widgets/search/content.php
+++ views/default/widgets/search/content.php
@@ -4,20 +4,24 @@
 
 	$form_body = elgg_view("input/text", array("name" => "q"));
 
-	list($type, $subtype) = explode(":" , $widget->types);
-	
-	if(!empty($type)){
+	$type = "";
+	$types = $widget->types;
+	if (!empty($types)) {
+		list($type, $subtype) = explode(":" , $types);
+	}
+
+	if (!empty($type)) {
 		$form_body .= elgg_view("input/hidden", array("name" => "search_type", "value" => "entities"));
 		$form_body .= elgg_view("input/hidden", array("name" => "entity_type", "value" => $type));
-		if(!empty($subtype)){
+		if (!empty($subtype)) {
 			$form_body .= elgg_view("input/hidden", array("name" => "entity_subtype", "value" => $subtype));
 		}
-	} 
-	
+	}
+
 	$form_body .= elgg_view("input/submit", array("value" => elgg_echo("search"), "class" => "hidden"));
-	
+
 	$form = elgg_view("input/form", array("body" => $form_body, "action" => "/search", "disable_security" => true, "class" => "search-advanced-widget-search-form"));
 
 	echo $form;
-	
+
 	echo "<div class='search-advanced-widget-search-results'></div>";
