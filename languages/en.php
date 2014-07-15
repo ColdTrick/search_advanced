4f5c40486b1b14f501b00239bf7e5e25ceb45b05
Jerome Bakker <jeabakker@coldtrick.com>
2014-07-15 16:57:45 +0200

    different placeholder

--- languages/en.php
+++ languages/en.php
@@ -1,6 +1,6 @@
 <?php
 
-$english = array(
+return array(
 	'search_advanced:settings:combine_search_results' => "Combine search results",
 	'search_advanced:settings:combine_search_results:info' => "This will show a combined list of the most recently created content based on your search query. There will be no more grouping on the 'all' page. Filtering with the side menu is still possible.",
 	'search_advanced:settings:enable_multi_tag' => "Enable multi tag search",
@@ -11,6 +11,5 @@ $english = array(
 	'search_advanced:content:title' => "Content",
 	'search_advanced:content:last_updated' => "Last updated",
 	'search_advanced:content:created' => "Created",
+	'search_asvanced:searchbox' => "Search"
 );
-	
-add_translation("en",$english);
