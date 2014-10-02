6d74008e636530bb22e2303776340bbfae8dec91
Jerome Bakker <jeabakker@coldtrick.com>
2014-10-02 16:25:23 +0200

    prepare for user profile field search

--- views/default/forms/search_advanced/search.php
+++ views/default/forms/search_advanced/search.php
@@ -17,6 +17,7 @@ if ($vars["container_guid"]) {
 }
 
 echo elgg_view("input/text", array("name" => "q", "value" => $vars["query"] , "class" => "ui-front"));
+echo elgg_view("input/submit", array("value" => elgg_echo("submit"), "class" => "hidden"));
 
 if (($user = elgg_get_logged_in_user_entity()) && elgg_trigger_plugin_hook("search_multisite", "search", array("user" => $user), false)) {
 	$current_value = 0;
@@ -28,4 +29,66 @@ if (($user = elgg_get_logged_in_user_entity()) && elgg_trigger_plugin_hook("sear
 	echo elgg_echo("search_advanced:multisite:label") . " ";
 	echo elgg_view("input/dropdown", array("name" => "multisite", "value" => $current_value, "options_values" => array(0 => elgg_echo("search_advanced:multisite:current"), 1 => elgg_echo("search_advanced:multisite:mine"))));
 	echo "</div>";
+}
+
+if (elgg_extract("type", $vars, false) === "user") {
+	$profile_fields = elgg_get_config("profile_fields");
+	$profile_field_values = elgg_get_plugin_setting("user_profile_fields", "search_advanced");
+	$profile_field_soundex_values = elgg_get_plugin_setting("user_profile_fields_soundex", "search_advanced");
+	if (empty($profile_field_values) || empty($profile_fields)) {
+		return;
+	}
+	
+	$submit_values = (array) get_input("search_advanced_profile_fields");
+	$profile_field_values = json_decode($profile_field_values, true);
+	$profile_field_soundex_submit_values = (array) get_input("search_advanced_profile_fields");
+	$profile_field_soundex_values = json_decode($profile_field_soundex_values, true);
+	$output = array();
+	
+	foreach ($profile_field_values as $profile_field) {
+		if (!isset($profile_fields[$profile_field])) {
+			continue;
+		}
+		
+		$lan_key = "profile:" . $profile_field;
+		$name = $profile_field;
+		if (elgg_echo($lan_key) !== $lan_key) {
+			$name = elgg_echo($lan_key);
+		}
+		
+		$row = array();
+		$row[] = $name;
+		$row[] = elgg_view("input/text", array(
+			"name" => "search_advanced_profile_fields[" . $profile_field . "]",
+			"value" => elgg_extract($profile_field, $submit_values)
+		));
+		if (in_array($profile_field, $profile_field_soundex_values)) {
+			$soundex_options = array(
+				"name" => "search_advanced_profile_fields_soundex[]",
+				"value" => $profile_field,
+				"label" => elgg_echo("search_advanced:users:profile_field:soundex")
+			);
+			if (in_array($profile_field, $profile_field_soundex_submit_values)) {
+				$soundex_options["checked"] = "checked";
+			}
+			
+			$row[] = elgg_view("input/checkbox", $soundex_options);
+		} else {
+			$row[] = "&nbsp;";
+		}
+		
+		$output[] = $row;
+	}
+	
+	if (!empty($output)) {
+		echo "<table class='search-advanced-user-profile-table mtm'>";
+		foreach ($output as $row) {
+			echo "<tr>";
+			echo "<td><label>" . $row[0] . "</label></td>";
+			echo "<td>" . $row[1] . "</td>";
+			echo "<td>" . $row[2] . "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	}
 }
--- views/default/plugins/search_advanced/settings.php
+++ views/default/plugins/search_advanced/settings.php
@@ -1,4 +1,7 @@
 <?php
+
+$plugin = elgg_extract("entity", $vars);
+
 $yesno_options = array(
 	"yes" => elgg_echo("option:yes"),
 	"no" => elgg_echo("option:no")
@@ -10,14 +13,67 @@ $separator_options = array(
 	"space" => elgg_echo("search_advanced:settings:multi_tag_separator:space"),
 );
 
+$profile_fields = elgg_get_config("profile_fields");
+$profile_field_values = $plugin->user_profile_fields;
+if (!empty($profile_field_values)) {
+	$profile_field_values = json_decode($profile_field_values, true);
+}
+$profile_field_soundex_values = $plugin->user_profile_fields_soundex;
+if (!empty($profile_field_soundex_values)) {
+	$profile_field_soundex_values = json_decode($profile_field_soundex_values, true);
+}
+
 echo "<label>" . elgg_echo('search_advanced:settings:combine_search_results') . "</label> ";
-echo elgg_view("input/dropdown", array("name" => "params[combine_search_results]", "options_values" => $noyes_options, "value" => $vars['entity']->combine_search_results));
+echo elgg_view("input/dropdown", array("name" => "params[combine_search_results]", "options_values" => $noyes_options, "value" => $plugin->combine_search_results));
 echo "<div class='elgg-subtext'>" . elgg_echo('search_advanced:settings:combine_search_results:info') . "</div>";
 
 echo "<label>" . elgg_echo('search_advanced:settings:enable_multi_tag') . "</label> ";
-echo elgg_view("input/dropdown", array("name" => "params[enable_multi_tag]", "options_values" => $noyes_options, "value" => $vars['entity']->enable_multi_tag));
+echo elgg_view("input/dropdown", array("name" => "params[enable_multi_tag]", "options_values" => $noyes_options, "value" => $plugin->enable_multi_tag));
 echo "<div class='elgg-subtext'>" . elgg_echo('search_advanced:settings:enable_multi_tag:info') . "</div>";
 
 echo "<label>" . elgg_echo('search_advanced:settings:multi_tag_separator') . "</label> ";
-echo elgg_view("input/dropdown", array("name" => "params[multi_tag_separator]", "options_values" => $separator_options, "value" => $vars['entity']->multi_tag_separator));
+echo elgg_view("input/dropdown", array("name" => "params[multi_tag_separator]", "options_values" => $separator_options, "value" => $plugin->multi_tag_separator));
 echo "<div class='elgg-subtext'>" . elgg_echo('search_advanced:settings:multi_tag_separator:info') . "</div>";
+
+echo "<label>" . elgg_echo('search_advanced:settings:user_profile_fields') . "</label> ";
+echo "<table class='elgg-table-alt'>";
+echo "<tr>";
+echo "<th>" . elgg_echo("search_advanced:settings:user_profile_fields:field") . "</th>";
+echo "<th class='center'>" . elgg_echo("search_advanced:settings:user_profile_fields:show_on_form");
+echo elgg_view("input/hidden", array("name" => "params[user_profile_fields]", "value" => 0)) . "</th>";
+echo "<th class='center'>" . elgg_echo("search_advanced:settings:user_profile_fields:use_soundex") . "</th>";
+echo "</tr>";
+foreach ($profile_fields as $metadata_name => $type) {
+	$lan_key = "profile:" . $metadata_name;
+	$name = $metadata_name;
+	if (elgg_echo($lan_key) !== $lan_key) {
+		$name = elgg_echo($lan_key);
+	}
+	
+	$name .= " (" . $type . ")";
+	
+	$show_field_options = array(
+		"name" => "params[user_profile_fields][]",
+		"value" => $metadata_name
+	);
+	if (in_array($metadata_name, $profile_field_values)) {
+		$show_field_options["checked"] = "checked";
+	}
+	$soundex_field_options = array(
+		"name" => "params[user_profile_fields_soundex][]",
+		"value" => $metadata_name
+	);
+	if (in_array($metadata_name, $profile_field_soundex_values)) {
+		$soundex_field_options["checked"] = "checked";
+	}
+	
+	echo "<tr>";
+	echo "<td><label>" . $name . "</label></td>";
+	echo "<td class='center'>" . elgg_view("input/checkbox", $show_field_options) . "</td>";
+	echo "<td class='center'>" . elgg_view("input/checkbox", $soundex_field_options) . "</td>";
+	echo "</tr>";
+	
+}
+echo "</table>";
+
+echo "<div class='elgg-subtext'>" . elgg_echo('search_advanced:settings:user_profile_fields:info') . "</div>";
