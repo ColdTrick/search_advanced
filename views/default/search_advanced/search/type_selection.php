417f1bbb3c31373e4b14efef349a72f0452431df
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-01-28 16:32:01 +0100

    fix(views): custom search types do not show up in type selection
    fixes #13

--- views/default/search_advanced/search/type_selection.php
+++ views/default/search_advanced/search/type_selection.php
@@ -1,11 +1,17 @@
 <?php
 
 $types = get_registered_entity_types();
+$custom_types = elgg_trigger_plugin_hook('search_types', 'get_types', array(), array());
 
 $current_selected = elgg_echo("all");
 
 $entity_type = get_input("entity_type");
 $entity_subtype = get_input("entity_subtype");
+$search_type = get_input("search_type");
+
+if (!in_array($search_type, $custom_types)) {
+	$search_type = "";
+}
 
 if (!array_key_exists($entity_type, $types)) {
 	$entity_type = "";
@@ -15,7 +21,13 @@ if (array_key_exists($entity_type, $types) && !in_array($entity_subtype, $types[
 	$entity_subtype = "";
 }
 
-if (!empty($entity_type)) {
+if (!empty($search_type) && ($search_type !== "entities")) {
+	$current_selected = elgg_echo("search_types:" . $search_type);
+	
+	echo elgg_view("input/hidden", array("name" => "search_type", "value" => $search_type));
+	echo elgg_view("input/hidden", array("name" => "entity_type", "disabled" => "disabled"));
+	echo elgg_view("input/hidden", array("name" => "entity_subtype", "disabled" => "disabled"));
+} elseif (!empty($entity_type)) {
 	echo elgg_view("input/hidden", array("name" => "search_type", "value" => "entities"));
 	echo elgg_view("input/hidden", array("name" => "entity_type", "value" => $entity_type));
 	
@@ -33,8 +45,6 @@ if (!empty($entity_type)) {
 	echo elgg_view("input/hidden", array("name" => "entity_type", "disabled" => "disabled"));
 	echo elgg_view("input/hidden", array("name" => "entity_subtype", "disabled" => "disabled"));
 }
-
-
 
 echo "<ul class='search-advanced-type-selection'>";
 echo "<li>";
