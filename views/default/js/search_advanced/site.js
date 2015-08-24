b6ac201c8ba1333c7177025822274e8ccfdc128c
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-08-24 14:59:20 +0200

    fixed: css and js issue with search_list menu

--- views/default/js/search_advanced/site.js
+++ views/default/js/search_advanced/site.js
@@ -1,9 +1,13 @@
 elgg.provide("elgg.search_advanced");
 
 elgg.search_advanced.init = function() {
-	$('.elgg-menu-item-list a').live('click', function(e) {
+	$('.elgg-menu-search-list > li > a').live('click', function(e) {
 		var $child_menu = $(this).next('.elgg-child-menu');
 		if ($child_menu.length) {
+			// hide other submenus
+			$('.elgg-menu-search-list .elgg-child-menu').hide();
+			
+			// show or hide submenu
 			$child_menu.toggle();
 		
 			e.stopPropagation();
@@ -12,7 +16,7 @@ elgg.search_advanced.init = function() {
 	});
 	
 	$(document).click(function() {
-		$('.elgg-menu-item-list .elgg-child-menu').hide();
+		$('.elgg-menu-search-list .elgg-child-menu').hide();
 	});
 }
 
--- views/default/search/layout.php
+++ views/default/search/layout.php
@@ -26,7 +26,7 @@ if (is_array($content)) {
 if (empty($content)) {
 	$content = elgg_view('search/no_results');
 } else {
-	$menu = elgg_view_menu('search_list', ['sort_by' => 'priority', 'class' => 'float-alt']);
+	$menu = elgg_view_menu('search_list', ['sort_by' => 'priority', 'class' => 'float-alt elgg-menu-hz']);
 	if (!empty($menu)) {
 		$menu = '<div class="clearfix">' . $menu . '</div>';
 		$content = $menu . $content;
