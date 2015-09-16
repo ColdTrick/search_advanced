f141b25fe115dd3b7eb719e146bbf965a40406a8
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-09-16 16:06:14 +0200

    chore: set correct class on sidebar search form button

--- views/default/search/sidebar.php
+++ views/default/search/sidebar.php
@@ -10,7 +10,7 @@ $filter = elgg_view('search/filter', $vars);
 if ($filter) {
 	echo $filter;
 	echo elgg_view('input/button', [
-		'class' => 'search-advanced-search-sidebar-button',
+		'class' => 'search-advanced-search-sidebar-button elgg-button-submit',
 		'value' => elgg_echo('search'),
 	]);
 }
