48aa807580110981c07b75425da5dd225395b851
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-11-10 09:22:10 +0100

    fixed: always show quotes in search title around search words

--- views/default/search/title.php
+++ views/default/search/title.php
@@ -1,14 +1,16 @@
-<?php 
+<?php
 
 $query = elgg_extract('query', $vars, '');
 $count = elgg_extract('count', $vars);
 $highlight_query = elgg_extract('highlight_query', $vars, true);
 
 // highlight search terms
-if (!empty($query) && $highlight_query) {
-	$searched_words = search_remove_ignored_words($query, 'array');	
-	$highlighted_query = search_highlight_words($searched_words, $query);
-	$query = "\"$highlighted_query\"";
+if (!empty($query)) {
+	if ($highlight_query) {
+		$searched_words = search_remove_ignored_words($query, 'array');
+		$query = search_highlight_words($searched_words, $query);
+	}
+	$query = "\"$query\"";
 }
 
 echo elgg_echo('search_advanced:results:title', [$count, $query]);
