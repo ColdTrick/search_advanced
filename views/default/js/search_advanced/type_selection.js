2763449af40f95f9155b66a9dfc5c1d66388c80c
Jerome Bakker <jeabakker@coldtrick.com>
2016-03-22 14:31:03 +0100

    fixed: using correct jquery function

--- views/default/js/search_advanced/type_selection.js
+++ views/default/js/search_advanced/type_selection.js
@@ -14,9 +14,9 @@ elgg.search_advanced.init_type_selection = function() {
 	$(document).on('click', '.search-advanced-type-selection-dropdown a', function(e) {
 		$(".search-advanced-type-selection > li > a").html($(this).html());
 
-		$(".elgg-search input[name='search_type']").attr("disabled", "disabled").val("all");
-		$(".elgg-search input[name='entity_type']").attr("disabled", "disabled").val("");
-		$(".elgg-search input[name='entity_subtype']").attr("disabled", "disabled").val("");
+		$(".elgg-search input[name='search_type']").prop("disabled", true).val("all");
+		$(".elgg-search input[name='entity_type']").prop("disabled", true).val("");
+		$(".elgg-search input[name='entity_subtype']").prop("disabled", true).val("");
 		
 		var rel = $(this).attr("rel");
 		
@@ -27,15 +27,15 @@ elgg.search_advanced.init_type_selection = function() {
 			if (input_vals[0]) {
 
 				if (input_vals[0] == "object" || input_vals[0] == "user" || input_vals[0] == "group") { 
-					$(".elgg-search input[name='search_type']").val("entities").removeAttr("disabled");
-					$(".elgg-search input[name='entity_type']").val(input_vals[0]).removeAttr("disabled");
+					$(".elgg-search input[name='search_type']").val("entities").prop("disabled", false);
+					$(".elgg-search input[name='entity_type']").val(input_vals[0]).prop("disabled", false);
 				} else {
-					$(".elgg-search input[name='search_type']").val(input_vals[0]).removeAttr("disabled");
+					$(".elgg-search input[name='search_type']").val(input_vals[0]).prop("disabled", false);
 				}
 			}
 
 			if (input_vals[1]) {
-				$(".elgg-search input[name='entity_subtype']").val(input_vals[1]).removeAttr("disabled");
+				$(".elgg-search input[name='entity_subtype']").val(input_vals[1]).prop("disabled", false);
 			}
 		}
 		
