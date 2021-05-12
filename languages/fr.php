d2df89d82c4a212411be93fd947c7428a0c0bfd7
Florian DANIEL aka Facyla <i.daniel.florian@gmail.com>
2021-05-12 09:05:02 +0200

    added: french translation

---
+++ languages/fr.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+	
+	'search_types:combined:objects' => "Contenu",
+	'search_types:combined:all' => "Tous les résultats",
+	
+	'search_advanced:settings:combine_search_results' => "Combiner les résultats de recherche",
+	'search_advanced:settings:combine_search_results:info' => "Cela affichera une liste combinée du contenu le plus récemment créé en fonction de votre requête de recherche. Il n’y aura plus de regroupement sur la page « Tout ». Le filtrage avec le menu latéral est encore possible.",
+	'search_advanced:settings:combine_search_results:objects' => "Combiner les objets",
+	'search_advanced:settings:combine_search_results:all' => "Tout combiner",
+	'search_advanced:settings:search_with_loader' => "Utiliser la recherche asynchrone",
+	'search_advanced:settings:search_with_loader:info' => "Affiche directement une animation de chargement sur la page de recherche et récupère les résultats via AJAX.",
+	'search_advanced:settings:enable_search_type_selection' => "Activer la sélection des types",
+	'search_advanced:settings:enable_search_type_selection:info' => "Affiche directement un sélecteur de type de résultat dans la zone de recherche.",
+
+	'search_advanced:settings:enable_autocomplete' => "Activer l'autocomplétion dans la barre de recherche",
+	'search_advanced:settings:enable_autocomplete:info' => "Commencer à écrire dans la boîte de recherche donnera quelques suggestions rapides.",
+	'search_advanced:settings:enable_autocomplete_content' => "Activer l'autocomplétion pour les contenus",
+	'search_advanced:settings:enable_autocomplete_content:info' => "Ajouter des liens vers les contenus aux résultats de l'autocomplétion.",
+	'search_advanced:settings:enable_autocomplete_helpers' => "Activer les aides à l'autocomplétion",
+	'search_advanced:settings:enable_autocomplete_helpers:info' => "Ajoute des liens d'aide contextuels aux résultats de l'autocomplétion.",
+	
+	'search_advanced:settings:profile_fields:disclaimer' => "Les paramètres ci-dessous ne s’appliquent qu’aux moteurs de recherche compatibles. Si Search Advanced ne contrôle pas les résultats de recherche, ces paramètres ne peuvent avoir aucun effet.",
+	'search_advanced:settings:profile_fields' => "Configurer des paramètres spécifiques aux champs de profil",
+	'search_advanced:settings:profile_fields:user' => "Champs de profil utilisateur",
+	'search_advanced:settings:profile_fields:group' => "Champs de profil de groupe",
+	'search_advanced:settings:profile_fields:metadata_search' => "Ne pas utiliser dans la recherche de profil",
+	'search_advanced:settings:profile_fields:field' => "Champ de profil",
+	'search_advanced:settings:user_profile_fields:show_on_form' => "Afficher sur le formulaire de recherche",
+	'search_advanced:settings:user_profile_fields:use_soundex' => "Utiliser Soundex",
+	'search_advanced:settings:user_profile_fields:info' => "Permettre aux utilisateurs d’affiner leur recherche d’utilisateurs en fonction des champs de profil. Actuellement, seuls les champs basés sur le texte sont pris en charge (texte, emplacement, url, etc.).",
+	'search_advanced:settings:query_required' => "Requête obligatoire",
+	'search_advanced:settings:query_required:help' => "Une requête est-elle nécessaire pour exécuter une recherche ? Si ce n’est pas le cas, la recherche affichera une liste avec du contenu récent (si elle est prise en charge).",
+	
+	'search_advanced:users:profile_field:soundex' => "Utiliser les sons similaires",
+	'search_advanced:searchbox' => "Rechercher",
+	'search_advanced:autocomplete:placeholder:all' => "[recherche tous les contenus]",
+	'search_advanced:autocomplete:placeholder:type' => "[type&nbsp;: %s]",
+	'search_advanced:autocomplete:placeholder:owner' => "[par&nbsp;: %s]",
+	'search_advanced:autocomplete:placeholder:container' => "[dans&nbsp;: %s]",
+	'search_advanced:widgets:search:edit:submit_behaviour' => "Soumettre un comportement",
+	'search_advanced:widgets:search:edit:submit_behaviour:show_in_widget' => "Afficher dans le widget",
+	'search_advanced:widgets:search:edit:submit_behaviour:go_to_search' => "Aller à la recherche",
+	'widgets:search_user:name' => "Formulaire de recherche utilisateur",
+	'widgets:search_user:description' => "Rechercher des utilisateurs avec les champs de recherche de métadonnées de profil",
+	'widgets:search_user:no_results' => "Ce widget ne peut pas être utilisé, car il n’y a pas de champs de profil configuré pour la recherche utilisateur.",
+
+	'search_advanced:results:title' => "%s résultats pour '%s'",
+	'search_advanced:results:empty:title' => "Résultats",
+	'search_advanced:results:empty_query' => "Veuillez fournir une requête de recherche",
+	
+	'search_advanced:members:sidebar:q:placeholder' => "Terme ou nom de recherche",
+	
+	'search_advanced:menu:search_list:list:title' => "Modifier l’apparence des résultats de recherche",
+	'search_advanced:menu:search_list:entity' => "Par défaut",
+	'search_advanced:menu:search_list:compact' => "Compact",
+
+	'search:filter:entities:user:title' => "Champ de profil",
+	'menu:search_types:header:object' => "Objets",
+	
+	'widgets:search:name' => "Rechercher",
+	'widgets:search:description' => "Rechercher des contenus sur le site",
+];
+
