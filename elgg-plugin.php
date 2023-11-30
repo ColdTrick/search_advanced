4a568dee405e16cb39877294cfe4a0edb950adc4
Jerôme Bakker <jeabakker@coldtrick.com>
2023-11-30 10:31:52 +0100

    chore: wrapping up v10.0

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -4,7 +4,7 @@ use Elgg\Router\Middleware\AjaxGatekeeper;
 
 return [
 	'plugin' => [
-		'version' => '9.0.1',
+		'version' => '10.0',
 		'dependencies' => [
 			'search' => [
 				'position' => 'after',
